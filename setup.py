7664b826c14eeb119127175cd4435b5d8c3f8bf5
Emanuele Di Giacomo <edigiacomo@arpae.it>
2022-09-23 06:31:06 +0200

    Removed django 2.2 support

--- setup.py
+++ setup.py
@@ -38,8 +38,6 @@ setup(
         'Environment :: Web Environment',
         'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
         'Framework :: Django',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.1',
         'Framework :: Django :: 3.2',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
